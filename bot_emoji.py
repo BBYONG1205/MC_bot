c77e6ee8077bdcea05e8b3a12650038f22187799
BBYONG1205 <bbyong1205@gmail.com>
2024-03-19 17:22:49 +0900

    이모지 py 분리

---
+++ bot_emoji.py
@@ -0,0 +1,19 @@
+def 이모지() :
+
+    이모지 = {
+        "고추": ':hot_pepper:',
+        "가지": ':eggplant:',
+        "마늘": ':garlic:',
+        "파인애플" : ':pineapple:',
+        "포도" : ':grapes:',
+        "토마토":':tomato:',
+        "홉" : ':olive:',
+        "배추" : ':leafy_green:',
+        "양배추" : ':green_circle:',
+        "옥수수" : ':corn:',
+        "생고기" : ':pig:',
+       # "조미료" : ':salt:',
+        "닭고기" : ':chicken:'
+    }      
+        
+    return 이모지
--- bot_event.py
+++ bot_event.py
@@ -26,8 +26,7 @@ async def 뵹뵹업데이트(client, message):
                 return
         
         if old_message.author == client.user and old_message.content.startswith('뵹뵹이님의 요청 내역을 정산하시겠습니까?'):
-            멤버_id = 370364396058181635
-            멤버 = await client.fetch_user(멤버_id)
+
             요청자 = "뵹뵹이님의 정산 요청 내역"
             프사 = "https://i.ibb.co/Y2h4FHW/image.webp"
             view = 뿅정산()
@@ -91,8 +90,6 @@ async def 머부업데이트(client, message):
         if old_message.author == client.user and old_message.content.startswith('김머부님의 요청 내역을 정산하시겠습니까?'):
 
             view = 머부정산()
-            멤버_id = 484314457313509376
-            멤버 = await client.fetch_user(멤버_id)
             프사 = "https://i.ibb.co/L9V6Zyq/image.webp"
             요청자 = "김머부님의 정산 요청 내역"
             embed = 정산_embed(요청자,프사)
